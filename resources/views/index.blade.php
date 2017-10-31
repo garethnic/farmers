de161209c2c17099dc883d271e4f98db166bfb1e
Gareth Nicholson <gareth.nic@gmail.com>
2017-10-31 17:39:12 +0200

    new analytics tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,13 +28,13 @@
         </script>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-97079954-2"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109027299-1"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'UA-97079954-2');
+            gtag('config', 'UA-109027299-1');
         </script>
     </head>
     <body>
