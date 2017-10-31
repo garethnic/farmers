491d11fe863a306215abcdc193f63b39ec0fa6aa
Gareth Nicholson <gareth.nic@gmail.com>
2017-10-31 21:41:59 +0200

    Change notification behaviour

--- resources/assets/js/push_message.js
+++ resources/assets/js/push_message.js
@@ -3,7 +3,11 @@ self.addEventListener('push', function(event) {
     const options = {
         body: 'Another farmer has been attacked.',
         icon: 'images/icons/icon-512x512.png',
-        badge: 'images/icons/icon-128x128.png'
+        badge: 'images/icons/icon-128x128.png',
+        vibrate: [500,110,500,110],
+        sound: '/sounds/9mm/mp3/9_mm.mp3',
+        tag: 'renotify',
+        renotify: true
     };
 
     event.waitUntil(self.registration.showNotification(title, options));
@@ -15,6 +19,6 @@ self.addEventListener('notificationclick', function(event) {
     event.notification.close();
 
     event.waitUntil(
-        clients.openWindow(window.location.hostname)
+        clients.openWindow('https://farmattacks.co.za')
     );
 });
