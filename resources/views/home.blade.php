c30c4ae873682695cb4dd972d857e5ca66804e88
Gareth Nicholson <gareth.nic@gmail.com>
2017-11-01 09:53:46 +0200

    Handle no year record in view correctly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,35 +26,41 @@
         </div>
 
         <div class="col-md-6">
-            <h2>Current Year ({{ $currentYear->year }})</h2>
+            @if($currentYear !== null)
+                <h2>Current Year ({{ $currentYear->year }})</h2>
+            @else
+                <h2>Create a new year</h2>
+            @endif
             <div class="form-group row">
                 <form action="/api/create-new-year" class="form-inline" method="post">
                     {{ csrf_field() }}
                     <button type="submit" class="btn btn-sm btn-primary">New Year</button>
                 </form>
             </div>
-            <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Assaults</label>
-                <label for="staticEmail" class="col-sm-10 col-form-label">{{ $currentYear->assaults }}</label>
-            </div>
-            <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Murders</label>
-                <label for="staticEmail" class="col-sm-10 col-form-label">{{ $currentYear->murders }}</label>
-            </div>
+            @if ($currentYear !== null)
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Assaults</label>
+                    <label for="staticEmail" class="col-sm-10 col-form-label">{{ $currentYear->assaults }}</label>
+                </div>
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Murders</label>
+                    <label for="staticEmail" class="col-sm-10 col-form-label">{{ $currentYear->murders }}</label>
+                </div>
 
-            <div class="form-group row">
-                <form action="/api/add-new-assault" class="form-inline" method="post">
-                    {{ csrf_field() }}
-                    <button type="submit" class="btn btn-warning">Add Assault</button>
-                </form>
+                <div class="form-group row">
+                    <form action="/api/add-new-assault" class="form-inline" method="post">
+                        {{ csrf_field() }}
+                        <button type="submit" class="btn btn-warning">Add Assault</button>
+                    </form>
 
-                <div class="px-4"></div>
+                    <div class="px-4"></div>
 
-                <form action="/api/add-new-murder" class="form-inline" method="post">
-                    {{ csrf_field() }}
-                    <button type="submit" class="btn btn-danger">Add Murder</button>
-                </form>
-            </div>
+                    <form action="/api/add-new-murder" class="form-inline" method="post">
+                        {{ csrf_field() }}
+                        <button type="submit" class="btn btn-danger">Add Murder</button>
+                    </form>
+                </div>
+            @endif
         </div>
     </div>
 </div>
